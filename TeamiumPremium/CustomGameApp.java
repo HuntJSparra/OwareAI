2c9a470928b472a5f3e45331e3ca5b9fdf158e99
Hunt J. Sparra <>
2021-03-04 17:58:07 -0500

    Added alpha-beta pruning A.I.

--- TeamiumPremium/CustomGameApp.java
+++ TeamiumPremium/CustomGameApp.java
@@ -24,11 +24,11 @@ public class CustomGameApp extends AIGameApp {
             // Set the players.
             Field defaultAccessibilityTOP_Player = AIGameApp.class.getDeclaredField("TOP_Player");
             defaultAccessibilityTOP_Player.setAccessible(true);
-            defaultAccessibilityTOP_Player.set(this, new BabyBaab());
+            defaultAccessibilityTOP_Player.set(this, new CarefulBaab());
 
             Field defaultAccessibilityBOT_Player = AIGameApp.class.getDeclaredField("BOT_Player");
             defaultAccessibilityBOT_Player.setAccessible(true);
-            defaultAccessibilityBOT_Player.set(this, new WiserOlderBaabPremium(4));
+            defaultAccessibilityBOT_Player.set(this, new PruniumWiserOlderBaabPremium(13));
 
             // Set the names for the (new) players.
             Player TOP_Player = (Player) defaultAccessibilityTOP_Player.get(this);
